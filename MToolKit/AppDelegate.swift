8c78384580d0bb846895fa63782a478f7ac3c6ee
gaoliang.miao <gaoliang5@staff.sina.com.cn>
2018-05-03 17:45:36 +0800

    Initial Commit

---
+++ MToolKit/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MToolKit
+//
+//  Created by gaoliang5 on 2018/5/3.
+//  Copyright © 2018年 gaoliang5. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
