2fbde162fd3dbd888fb99f829d1f738781d72e95
gaoliang.miao <gaoliang5@staff.sina.com.cn>
2018-05-15 10:08:23 +0800

    【布局常量】class LayoutConst

---
+++ MToolKit/LayoutConst.swift
@@ -0,0 +1,15 @@
+//
+//  LayoutConst.swift
+//  MToolKit
+//
+//  Created by gaoliang5 on 2018/5/15.
+//  Copyright © 2018年 gaoliang5. All rights reserved.
+//
+
+import Foundation
+
+class LayoutConst {
+	static let edgePadding : Int = 20
+	static let spacePadding : Int = 10
+
+}
