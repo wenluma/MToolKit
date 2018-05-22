280c6284243208b9add3db2807a0103314a218a8
gaoliang.miao <gaoliang5@staff.sina.com.cn>
2018-05-22 10:39:57 +0800

    【string regular】regex 正则表达式

---
+++ MToolKit/StringRegularExtension.swift
@@ -0,0 +1,26 @@
+//
+//  StringRegularExtension.swift
+//  MToolKit
+//
+//  Created by gaoliang5 on 2018/5/22.
+//  Copyright © 2018年 gaoliang5. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - mtk_matchRegular 正则的表达式使用入口
+extension String {
+	func mtk_matchRegular(_ pattern: String) -> [String] {
+		do {
+			let regex = try NSRegularExpression(pattern: pattern)
+			let results = regex.matches(in: self,
+										range: NSRange(self.startIndex..., in: self))
+			return results.map {
+				String(self[Range($0.range, in: self)!])
+			}
+		} catch let error {
+			print("invalid regex: \(error.localizedDescription)")
+			return []
+		}
+	}
+}
